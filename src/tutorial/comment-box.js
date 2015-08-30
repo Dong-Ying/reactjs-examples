05ca4e35f1790ac11264718070ec1d3c2adc08c0
Dong Ying <ydong@thoughtworks.com>
2015-08-30 11:56:36 +0800

    add comment box

---
+++ src/tutorial/comment-box.js
@@ -0,0 +1,46 @@
+var CommentBox = React.createClass({
+        loadCommentsFromServer: function () {
+            var data = [
+                {author: "Pete Hunt", text: "This is one comment"},
+                {author: "Jordan Walke", text: "This is *another* comment"}
+            ];
+            this.setState({data: data});
+        },
+        handleCommentSubmit: function (comment) {
+            var comments = this.state.data;
+            var newComments = comments.concat([comment]);
+            this.setState({data: newComments});
+
+            //$.ajax({
+            //    url: this.props.url,
+            //    dataType: 'json',
+            //    type: 'POST',
+            //    data: comment,
+            //    success: function(data) {
+            //        this.setState({data: data});
+            //    }.bind(this),
+            //    error: function(xhr, status, err) {
+            //        console.error(this.props.url, status, err.toString());
+            //    }.bind(this)
+            //});
+        }
+        ,
+        getInitialState: function () {
+            return {data: []};
+        }
+        ,
+        componentDidMount: function () {
+            this.loadCommentsFromServer();
+        }
+        ,
+        render: function () {
+            return (
+                <div className="commentBox">
+                    <h1>Comments</h1>
+                    <CommentList data={this.state.data}/>
+                    <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+                </div>
+            );
+        }
+    })
+    ;
